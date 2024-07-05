9f560f5c8308f39270ac15fae84327afe10b1bcc
Your Name <princesslorna2000@gmail.com>
2024-07-05 17:25:58 +0300

    using find

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(records) {
+    let winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+// for example
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ];
+  console.log(superbowlWin(record));
+  
+  
